c15886f92decc0ad4dfa329b62c4a0a90fb0101a
MANOJ.V_CSE <160569397+Manojvcse@users.noreply.github.com>
2024-07-20 12:53:37 +0530

    Add files via upload

---
+++ Voice_Control_Home_Automation_Alexa/arduino_secrets.h
@@ -0,0 +1,3 @@
+#define SECRET_SSID "Samsung Galaxy F12"  //Enter WIFI NAME
+#define SECRET_PASS "manoj051"  //Enter WIFI PASSWORD
+#define SECRET_DEVICE_KEY "4r6zAw0hPJbrbMKVpZivZ10CY"  //Enter Secret Key
